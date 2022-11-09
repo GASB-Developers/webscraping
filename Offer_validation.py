b43bc38a4c622d5bf7f7dd16e11c83850e64defe
Marvin Hildebrandt <mhildebrandt@techfak.uni-bielefeld.de>
2022-11-09 22:38:44 +0100

    Added identification of differently written company for duplicate search

--- Offer_validation.py
+++ Offer_validation.py
@@ -13,6 +13,48 @@ job_types_dict = dict(Full_Time=["Part-time", "Part time", "part time", "part-ti
                                "postdoctoral", "Postdoc"],
                       StartUp=["StartUp", "Startup"])
 
+
+# applies several string operations to the title of a job offer in order to make comparison easier
+def comparable_title(title):
+    # convert to lower case
+    title1 = title.lower()
+    # remove e.g. (m/w/d) addition
+    title1 = re.sub(" \(.{3,7}\)", "", title1)
+    # remove mfd addition that is not covered by regular expression
+    title1 = title1.replace(" mfd", "")
+    # remove special/uninformative characters
+    title1 = title1.replace(",", "")
+    title1 = title1.replace(" -", "")
+    title1 = title1.replace("-", " ")
+    title1 = title1.replace(" –", "")
+    title1 = title1.replace("...", "")
+    title1 = title1.replace("…", "")
+    title1 = title1.replace(".", "")
+    title1 = title1.replace("\"", "")
+    title1 = title1.replace("“", "")
+    title1 = title1.replace("/", "")
+    title1 = title1.replace("in", "")
+    return title1
+
+
+def same_title(first, second):
+    title1 = comparable_title(first).split(" ")
+    title2 = comparable_title(second).split(" ")
+    # check if all terms contained in one title can be found in the other one
+    # this relationship does not need to be bidirectional, in case one title is cut-off for example
+    one_in_two = True
+    for term in title1:
+        if term not in title2:
+            one_in_two = False
+            break
+    two_in_one = True
+    for term in title2:
+        if term not in title1:
+            two_in_one = False
+            break
+    return one_in_two or two_in_one
+
+
 ### Definition of data-type to store job offers ###
 class JobOffer:
     def __init__(self, title, job_type, description, application_address_url, company, location, post_date):
@@ -39,7 +81,7 @@ class JobOffer:
         self.post_date = post_date
 
     def __str__(self):
-        return "Title: {}\nDescription: {}\nCompany: {}\nLocation: {}\nJob-Type: {}\nApplication-URL: {}\n"\
+        return "Title: {}\nDescription: {}\nCompany: {}\nLocation: {}\nJob-Type: {}\nApplication-URL: {}\n" \
             .format(self.title, tr.shorten(self.description, width=100, placeholder=" [...]"),
                     self.company, self.location, ", ".join(self.job_type), self.application_address_url, self.post_date)
 
@@ -50,54 +92,11 @@ class JobOffer:
                and self.location == other.location and self.post_date == other.post_date
 
     # decide whether two offers are duplicates of each other TODO: make duplicate identification more precise
-    # TODO: Consider case when the same company is written differently
     def is_duplicate(self, other):
         # does the case occur, that one company offers two distinct jobs with the same title?
         return self.description == other.description \
-               or self.has_same_title(other) and self.location == other.location \
-               or self.has_same_title(other) and self.company == other.company
-
-    # applies several string operations to the title of a job offer in order to make comparison easier
-    def comparable_title(self):
-        # convert to lower case
-        title1 = self.title.lower()
-        # remove e.g. (m/w/d) addition
-        title1 = re.sub(" \(.{3,7}\)", "", title1)
-        # remove mfd addition that is not covered by regular expression
-        title1 = title1.replace(" mfd", "")
-        # remove special/uninformative characters
-        title1 = title1.replace(",", "")
-        title1 = title1.replace(" -", "")
-        title1 = title1.replace("-", " ")
-        title1 = title1.replace(" –", "")
-        title1 = title1.replace("...", "")
-        title1 = title1.replace("…", "")
-        title1 = title1.replace(".", "")
-        title1 = title1.replace("\"", "")
-        title1 = title1.replace("“", "")
-        title1 = title1.replace("/", "")
-        title1 = title1.replace("in", "")
-        return title1
-
-    # check whether two job offers have (nearly) the same title
-    # when the order of words is altered, but all can be found in the other one,
-    # both titles are still considered to be the same
-    def has_same_title(self, other):
-        title1 = self.comparable_title().split(" ")
-        title2 = other.comparable_title().split(" ")
-        # check if all terms contained in one title can be found in the other one
-        # this relationship does not need to be bidirectional, in case one title is cut-off for example
-        one_in_two = True
-        for term in title1:
-            if term not in title2:
-                one_in_two = False
-                break
-        two_in_one = True
-        for term in title2:
-            if term not in title1:
-                two_in_one = False
-                break
-        return one_in_two or two_in_one
+               or same_title(self.title, other.title) and self.location == other.location \
+               or same_title(self.title, other.title) and same_title(self.company, other.company)
 
     # generate string for .csv file, representing object
     def csv_line(self):
