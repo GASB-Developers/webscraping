11214ad3cfe422cf05183b27b172e04685460205
Marvin Hildebrandt <mhildebrandt@techfak.uni-bielefeld.de>
2022-11-24 23:58:14 +0100

    Experimented with NLP approach suggested by Jonny

--- data_science_test.py
+++ data_science_test.py
@@ -1,12 +1,37 @@
 import csv
 import nltk
-from sklearn.feature_extraction.text import CountVectorizer
-#nltk.download('punkt')
+import numpy as np
+import seaborn as sns
+import matplotlib.pylab as plt
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-import_file = "export_files/export_22-11-02.csv"
+import_file = "export_files/export_22-11-24.csv"
 
-with open(import_file, "r", newline="", encoding="utf-8") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=";")
-    for line in csv_reader:
-        print(nltk.tokenize.word_tokenize(line[0].lower()))
-        #print(line[0])
+
+def calculate_similarity(column, file=import_file):
+    dat = pd.read_csv(file, delimiter=';')
+    data_list = dat[column].to_list()
+    tfidf = TfidfVectorizer().fit_transform(data_list)
+    pairwise_similarity = tfidf * tfidf.T
+    similarity_array = pairwise_similarity.toarray()
+    return similarity_array
+
+
+combined_similarity_array = calculate_similarity("title")
+combined_similarity_array += calculate_similarity("description")
+combined_similarity_array += calculate_similarity("company")
+
+mean_similarity = combined_similarity_array/3
+
+ax = sns.heatmap(mean_similarity, linewidth=0.5)
+ax.set_title('combined')
+plt.show()
+
+threshold = 0.3
+
+x_flat = mean_similarity.flatten()
+x_flat.sort()
+plt.plot(x_flat)
+plt.axhline(y=threshold, color='r', linestyle='-')
+plt.show()
