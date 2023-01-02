534befef00d857d0d90aca1988e4a9fdc5b0ec09
Marvin Hildebrandt <mhildebrandt@techfak.uni-bielefeld.de>
2023-01-02 17:59:17 +0100

    Extended data visualization for parameter optimization from Jonathan Funk to all export files

--- data_science_test.py
+++ data_science_test.py
@@ -11,29 +11,38 @@ import_file = "export_files/export_22-11-24.csv"
 import_files = os.listdir("export_files/")
 
 
-#print(import_files)
+print(import_files)
+title_list = []
+description_list = []
+company_list = []
+for file in import_files:
+    dat = pd.read_csv("export_files/" + file, delimiter=';')
+    title_list.extend(dat["title"].to_list())
+    description_list.extend(dat["description"].to_list())
+    company_list.extend(dat["company"].to_list())
 
 
-def calculate_similarity(column, file=import_file):
-    dat = pd.read_csv(file, delimiter=';')
-    data_list = dat[column].to_list()
+def calculate_similarity(data_list):
     tfidf = TfidfVectorizer().fit_transform(data_list)
     pairwise_similarity = tfidf * tfidf.T
     similarity_array = pairwise_similarity.toarray()
     return similarity_array
 
 
-combined_similarity_array = calculate_similarity("title")
-combined_similarity_array += calculate_similarity("description")
-combined_similarity_array += calculate_similarity("company")
+combined_similarity_array = calculate_similarity(title_list)
+combined_similarity_array += calculate_similarity(description_list)
+# When using all export files: companies have to be removed from analysis as the column is missing in older files
+#combined_similarity_array += calculate_similarity(company_list)
 
-mean_similarity = combined_similarity_array/3
+#mean_similarity = combined_similarity_array/3
+mean_similarity = combined_similarity_array/2
 
-print(mean_similarity)
+# When using all export files: Heatmap is not very meaningful
+#print(mean_similarity)
 
-ax = sns.heatmap(mean_similarity, linewidth=0.5)
-ax.set_title('combined')
-plt.show()
+#ax = sns.heatmap(mean_similarity)
+#ax.set_title('combined')
+#plt.show()
 
 threshold = 0.3
 
